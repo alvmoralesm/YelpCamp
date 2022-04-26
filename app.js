10b26ea3fc0a65b44def4036165342f61d34bd29
Alfajor <alv.moralesm@duocuc.cl>
2022-04-26 12:13:34 -0400

    add start script

--- app.js
+++ app.js
@@ -162,6 +162,7 @@ app.use((err, req, res, next) => {
   res.status(status).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("LISTENING!");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`LISTENING! Port: ${port}`);
 });
