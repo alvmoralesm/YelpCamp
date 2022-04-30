26a42945dd004fae5e38791a4b5cac1ca7d7fb38
Alfajor <alv.moralesm@duocuc.cl>
2022-04-30 01:01:49 -0400

    Refactor: Admin can now delete reviews from other users + Display review form depending on user signed in and user role added

--- middleware.js
+++ middleware.js
@@ -56,7 +56,7 @@ module.exports.isAuthor = async (req, res, next) => {
 module.exports.canReview = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
-  if (!campground.author.equals(req.user.id) || req.user.isAdmin === true) {
+  if (!campground.author.equals(req.user.id) || req.user.isAdmin) {
     next();
   } else if (campground.author.equals(req.user.id)) {
     console.log(campground.author.equals(req.user.id));
@@ -68,9 +68,10 @@ module.exports.canReview = async (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
   const { id, reviewId } = req.params;
   const review = await Review.findById(reviewId);
-  if (!review.author.equals(req.user.id)) {
+  if (review.author.equals(req.user.id) || req.user.isAdmin) {
+    next();
+  } else if (!review.author.equals(req.user.id)) {
     req.flash("error", "You do not have permission to do that!");
     return res.redirect(`/campgrounds/${id}`);
   }
-  next();
 };
